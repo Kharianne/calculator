5a970a1be85c6dad3f3bee72f3acdd5e69130763
Kharianne <katerinamrkackova@gmail.com>
2017-08-17 12:56:45 +0200

    handling negatives and greater than 1000

--- calculator.py
+++ calculator.py
@@ -1,12 +1,13 @@
 import re
 
+
 class Calculator:
 
     def add(self, numbers: str)-> int:
         if numbers.startswith('//'):
-            delimeter, numbers = self.determine_delimeter(numbers)
+            delimiter, numbers = self.determine_delimiter(numbers)
             numbers = numbers.strip()
-            numbers = numbers.replace('\n', ',').replace(delimeter, ',')
+            numbers = numbers.replace('\n', ',').replace(delimiter, ',')
         else:
             numbers = numbers.strip()
             numbers = numbers.replace('\n', ',')
@@ -14,18 +15,19 @@ class Calculator:
             result = 0
         else:
             digits = numbers.split(',')
-            result = 0
-            for digit in digits:
-                try:
-                    result += int(digit)
-                except ValueError:
-                    result = result
-                    continue
+            # Handling of exception due to invalid input is responsibility of calling code
+            # Strip whitespaces to allow input like "1,,2" (empty string should be 0)
+            digits = [int(digit) for digit in digits if digit.strip() and int(digit) <= 1000]
+            negatives = [digit for digit in digits if digit < 0]
+            if negatives:
+                raise ValueError('negatives not allowed ' + ', '.join(str(x) for x in negatives))
+            result = sum(digits)
         return result
 
-    def determine_delimeter(self, numbers: str)->tuple:
+    # I put it outside of add function to be clearer.
+    def determine_delimiter(self, numbers: str)->tuple:
         regex = re.compile('\[(.*?)\]')
-        delimeter = re.findall(regex, numbers)[0]
+        delimiter = re.findall(regex, numbers)[0]
         numbers = numbers.split('\n')[1]
-        return delimeter, numbers
+        return delimiter, numbers
 
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import calculator
+import pytest
 
 calc = calculator.Calculator()
 
@@ -30,7 +31,8 @@ def test_05():
 
 def test_06():
     """String with letters test"""
-    assert calc.add("1,2,Lersre,2,D") == 5
+    with pytest.raises(ValueError):
+        calc.add("1,2,Lersre,2,D")
 
 
 def test_07():
@@ -39,9 +41,37 @@ def test_07():
 
 
 def test_08():
-    """Custon delimeter"""
-    assert calc.determine_delimeter("//[;]\n1;2")[0] == ';'
+    """Custom delimeter"""
+    assert calc.determine_delimiter("//[;]\n1;2")[0] == ';'
+
 
 def test_09():
-    """Custon delimeter add"""
+    """Custom delimeter add"""
     assert calc.add("//[;]\n1;2") == 3
+
+
+def test_10():
+    """Two delimeters in row (test empty string input)"""
+    assert calc.add("1, ,2") == 3
+
+
+def test_11():
+    """ValueError test"""
+    with pytest.raises(ValueError) as info:
+        calc.add("1,-2,-3")
+    assert str(info.value) == 'negatives not allowed -2, -3'
+
+
+def test_12():
+    """Number bigger than 1000"""
+    assert calc.add("1001,1,3") == 4
+
+
+def test_13():
+    """Number equal 1000"""
+    assert calc.add("1000,2,5") == 1007
+
+
+def test_14():
+    """Multiple character delimiter"""
+    assert calc.add("//[***]\n1***2***3") == 6
